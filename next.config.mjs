6e5c9a5e8a5a58b4d6a9025bc1a297579b203b72
vonOtto <143093151+vonOtto@users.noreply.github.com>
2024-05-31 09:24:01 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 import nextTranslate from 'next-translate-plugin';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = nextTranslate({
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-});
+  missingSuspenseWithCSRBailout: false,
+  ...nextTranslate({
+    // other next-translate options if any
+  }),
+};
 
 export default nextConfig;
